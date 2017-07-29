96c2ce9c30569febd9dd42ed44642b2ee5eca6d7
maxwellito <duluc.maxime@gmail.com>
2017-07-29 16:47:45 +0100

    Update layout on newCanvasForm component

--- src/components/newCanvasForm/newCanvasForm.component.js
+++ src/components/newCanvasForm/newCanvasForm.component.js
@@ -8,23 +8,39 @@ Vue.component('new-canvas-form', {
     }
   },
   methods: {
+    create: function () {
+      this.$emit('new-canvas', {
+        width: this.width,
+        height: this.height,
+        isLandscape: this.isLandscape
+      })
+    },
+    cancel: function () {
+      this.$emit('back')
+    }
   },
   template: '\
-    <div class="row">\
-      <label>\
-        <div class="field">\
-          <input class="field-input" type="number" v-model="width" min="1"/>\
-          <span class="field-icon">↔</span>\
-        </div>\
-        <span>Map width</span>\
-      </label>\
-      <label>\
-        <div class="field">\
-          <input class="field-input" type="number" v-model="height" min="1"/>\
-          <span class="field-icon">↕</span>\
-        </div>\
-        <span>Map height</span>\
-      </label>\
-      <grid-orientation-picker @update:isLandscape="val => isLandscape = val"></grid-orientation-picker>\
+    <div>\
+      <div class="row">\
+        <label>\
+          <div class="field">\
+            <input class="field-input" type="number" v-model="width" min="1"/>\
+            <span class="field-icon">↔</span>\
+          </div>\
+          <span>Map width</span>\
+        </label>\
+        <label>\
+          <div class="field">\
+            <input class="field-input" type="number" v-model="height" min="1"/>\
+            <span class="field-icon">↕</span>\
+          </div>\
+          <span>Map height</span>\
+        </label>\
+        <grid-orientation-picker @update:isLandscape="val => isLandscape = val"></grid-orientation-picker>\
+      </div>\
+      <div>\
+        <button class="button">Back</button>\
+        <button class="button">GO!</button>\
+      </div>\
     </div>'
 })
